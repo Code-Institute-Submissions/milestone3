cebca381151c3db11ecde13e70d075c246788cb6
Phil Surgenor <philsurgenor87@gmail.com>
2018-05-08 15:25:40 +0000

    tests complete to verify username

--- game-tdd/verify_username_test.py
+++ game-tdd/verify_username_test.py
@@ -1,6 +1,21 @@
 # Tests will be carried out to determine if a username is unique after reading users.txt file
 
-
+def verify_username(username):
+    if username == "":
+        return False
+    
+    with open("users_test.txt", "r") as file:
+        users = file.read().splitlines()
+        
+    for user in users:
+        if user == username:
+            return False
+            
+    file = open("users_test.txt", "a")
+    file.write(username + "\n")
+    file.close()
+    return True
+        
 
 
 
@@ -8,3 +23,4 @@ assert verify_username("") == False, "username was left blank"
 assert verify_username("bob123") == True, "Username was added to users.txt"
 assert verify_username("skycat92") == False, "Username already existss"
 
+print("All Tests Pass")
