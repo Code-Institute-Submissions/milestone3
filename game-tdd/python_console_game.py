c16dbfe0df54be812d970dca218354b87d95463d
Phil Surgenor <philsurgenor87@gmail.com>
2018-05-09 10:15:08 +0000

    Username functionality added

--- game-tdd/python_console_game.py
+++ game-tdd/python_console_game.py
@@ -1,20 +1,33 @@
-def add_username():
-    username = input("Enter a Username\n>")
+def verify_username(username):
     if username == "":
         print("Username Cannot Be Left Blank!\n")
-    else:
+        return False
+    
+    with open("users_test.txt", "r") as file:
+        users = file.read().splitlines()
+        
+    for user in users:
+        if user == username:
+            print("Username already used, please choose another\n")
+            return False
+        else: return True
+            
+    
+def add_username():
+    username = input("Enter a Username\n>")
+    
+    if verify_username(username):
+        file = open("users_test.txt", "a")
+        file.write(username + "\n")
+        file.close()
+        
         print("\nWelcome " + username + "\n")
         return True
     
 
-
 def game_loop():
-    while True:
-        add_username()
-        if add_username():
-            print("yay!")
-        else:
-            print("nay!")
+    add_username()
+    
         
 
 game_loop()
