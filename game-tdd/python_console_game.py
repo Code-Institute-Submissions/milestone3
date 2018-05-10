b5d185c0b4a060ca427d41f8f8c4d805e8576083
Phil Surgenor <philsurgenor87@gmail.com>
2018-05-10 16:35:45 +0000

    Question and Answers Functionality Added

--- game-tdd/python_console_game.py
+++ game-tdd/python_console_game.py
@@ -1,3 +1,4 @@
+# Check wether username given is blank, already taken or valid
 def verify_username(username):
     if username == "":
         print("Username Cannot Be Left Blank!\n")
@@ -10,9 +11,9 @@ def verify_username(username):
         if user == username:
             print("Username already used, please choose another\n")
             return False
-        else: return True
-            
-    
+    return True
+
+#Takes username as an input value, if valid it adds it to users_test.txt file
 def add_username():
     username = input("Enter a Username\n>")
     
@@ -23,11 +24,42 @@ def add_username():
         
         print("\nWelcome " + username + "\n")
         return True
+    else: add_username()
+
+
+# This function will take questions from questions_test.txt and loop through them.
+def questions():
+    print("It's question time!\n")
+    questions = []
+    answers = []
+    
+    with open("questions_test.txt", "r") as file:
+        lines = file.read().splitlines()
+        
+    for i, text in enumerate(lines):
+        if i%2 == 0:
+            questions.append(text)
+        else:
+            answers.append(text)
+            
+    questions_and_answers = zip(questions, answers)
+    question_num = 0
+            
+    for question, answer in questions_and_answers:
+        question_num += 1
+        guess = input("Question {0} > {1}\n".format(question_num, question))
+        
+        if guess == answer:
+            print("Correct")
+        else:
+            print("Wrong!")
+            
     
 
+# Game functionality
 def game_loop():
     add_username()
-    
+    questions()
         
 
 game_loop()
