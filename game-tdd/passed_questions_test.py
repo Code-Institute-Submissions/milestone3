8d29942dc9dc6e9b1117266401243d5c658203c7
Phil Surgenor <philsurgenor87@gmail.com>
2018-08-18 18:22:16 +0000

    Second Test, 3 Passed questions gives 4 lives left

--- game-tdd/passed_questions_test.py
+++ game-tdd/passed_questions_test.py
@@ -5,7 +5,18 @@ def lives_left(x):
         lives = 5
         print( "You still have {0} lives left".format(lives))
         return lives
-
+    
+    lives = 5   
+    for i in range(passed_questions):
+        
+        i += 1
+        print(i%3)
+        
+        if( i%3 == 0 ):
+            lives -=1
+            
+            print("You Have {0} Lives Left".format(lives))
+    return lives
 
 
 
@@ -14,6 +25,8 @@ def test_if_equal(actual, expected):
     assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
 
 test_if_equal(lives_left(0),5)
+test_if_equal(lives_left(3),4)
+
 
 
 
