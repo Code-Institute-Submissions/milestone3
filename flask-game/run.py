c5e409597bad91e6ace39170a179cd4e940a27b9
Phil Surgenor <philsurgenor87@gmail.com>
2018-09-13 18:28:38 +0000

    Added routes for about, instructions and contact

--- flask-game/run.py
+++ flask-game/run.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
     
+@app.route('/about')
+def about():
+    return render_template("about.html")
+    
+@app.route('/instructions')
+def instructions():
+    return render_template("instructions.html")
+    
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+    
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
